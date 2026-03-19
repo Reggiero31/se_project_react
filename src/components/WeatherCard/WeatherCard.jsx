b3e965efe8e0310754c62b56e53bb9f55907d94b
Reginald Robinson <reggierobinson012@gmail.com>
2026-03-19 16:21:38 -0500

    se_project_react

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -3,7 +3,7 @@ import sunnyday from "../../assets/day/sunny.png"
 import { weatherOptions } from "../../utils/constants";
 
 function WeatherCard({weatherData}) {
-    return <section className="weather-Card">
+    return <section className="weather-card">
          <p className="weather-card__temp"> 75 &deg; F</p>
         <img src={sunnyday} alt="sunny" className="weather-card__image" />
     </section>
